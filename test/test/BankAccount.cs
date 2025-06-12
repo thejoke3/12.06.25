1e9f53760039072a13753ce5ac48a7f1d28fc029
thejoke3 <167237678+thejoke3@users.noreply.github.com>
2025-06-12 11:12:39 +0300

    Add files via upload

---
+++ test/test/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class BankAccount
+    {
+        private int id;
+        private double balance;
+
+        public int ID { get; set; }
+        private double Balance { get; set; }
+
+        public BankAccount()
+        {
+            this.id = ID;
+            this.balance = Balance;
+            Deposit(Balance);
+            Withdraw(Balance);      
+        }
+        public string ToString()
+        {
+            
+          return $"Balance: {Balance.ToString()}";
+
+        }   
+       public  void Deposit(double amount)
+       {
+            Balance = Balance + amount;
+           
+       }
+
+        public void Withdraw(double amount)
+        {
+            Balance = Balance - amount;
+           
+        }
+    }
+}
---
+++ test/test/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          BankAccount acc = new BankAccount();
+            acc.ID = 1;
+            acc.Deposit(15);
+            acc.Withdraw(5);
+            
+            Console.WriteLine(acc.ToString());
+        }
+    }
+}
